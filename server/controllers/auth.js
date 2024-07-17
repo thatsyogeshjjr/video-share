6bae412c134206d91c9fdc894700d1ef9569a64e
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-07-18 00:06:25 +0530

    jwt and cookie implemented

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import dotenv from "dotenv";
 import { createError } from "../error.js";
+
+dotenv.config();
 
 export const signup = async (req, res, next) => {
   try {
@@ -20,12 +24,15 @@ export const signin = async (req, res, next) => {
     const user = await User.findOne({ name: req.body.name });
     if (!user) return next(createError(404, "User not found!"));
     let isCorrect = await bcrypt.compare(req.body.password, user.password);
-    if (isCorrect == false) {
-      return next(createError(400, "Wrong credentials!"));
-    }
-    if (isCorrect) {
-      console.log("password good");
-    }
+    if (isCorrect == false) return next(createError(400, "Wrong credentials!"));
+
+    // if credentials are valid
+    const { password, ...non_password_data } = user._doc;
+    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(non_password_data);
   } catch (error) {
     next(error);
   }
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.js";
 import commentRoutes from "./routes/comments.js";
 import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/videos.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -22,21 +23,22 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
 
-// app.use((err, req, res, next) => {
-//   const status = err.status || 500;
-//   const message = err.message || "Something went wrong";
-//   return res.status(status).json({
-//     success: false,
-//     status,
-//     message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
 app.listen(PORT, () => {
   connect();
