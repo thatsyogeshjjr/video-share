4d2fe2a23cec05ab3692c37fd47d37eb9fe36ed0
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-07-21 00:08:44 +0530

    added comment controllers

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,1 +1,43 @@
+import { createError } from "../error.js";
+import Comment from "../models/Comment.js";
+import Video from "../models/Video.js";
 
+export const addComment = async (req, res, next) => {
+  const newComment = new Comment({
+    ...req.body,
+    userId: req.user.id,
+  });
+  try {
+    const savedComment = await newComment.save();
+    res.status(200).json(savedComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getComment = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ videoId: req.params.videoId }).limit(
+      40
+    );
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(req.params.id);
+
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("The comment was deleted.");
+    } else {
+      next(createError(403, "Aren't authorized to delete this comment"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,5 +1,15 @@
 import express from "express";
+import { verifyToken } from "../verifyToken.js";
+import {
+  addComment,
+  deleteComment,
+  getComment,
+} from "../controllers/comment.js";
 
 const router = express.Router();
+
+router.post("/", verifyToken, addComment);
+router.get("/:videoId", verifyToken, getComment);
+router.delete("/:id", verifyToken, deleteComment);
 
 export default router;
